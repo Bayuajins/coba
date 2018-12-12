1133845c57512652976e5e75b5bc618df4733aa2
Bayuajins <Bayuajins@gmail.com>
2018-12-12 22:16:29 +0700

    first commit

---
+++ coba.py
@@ -0,0 +1,7 @@
+name=input("nama:")
+print("now enter your age")
+age=int(input("age:"))
+days=age*365
+minutes=age*525948
+second=age*31556926
+print(name,"have been alive for",days,"days",minutes,"minutes and",seconds,"seconds! now!")
